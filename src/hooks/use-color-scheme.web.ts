0ed612d634f1951e8d24e97a49b27c0f46c03ff4
Lukas Pikora <lpikora@gmail.com>
2026-08-18 17:43:16 +0200

    feat: implement hydration-safe color scheme hook for web platform

--- src/hooks/use-color-scheme.web.ts
+++ src/hooks/use-color-scheme.web.ts
@@ -2,13 +2,16 @@ import { useEffect, useState } from "react";
 import { useColorScheme as useRNColorScheme } from "react-native";
 
 /**
- * To support static rendering,  s  dthis v alue needs to be re-calculated on the client side for web
+ * To support static rendering, this value needs to be re-calculated on the client side for web
  */
 export function useColorScheme() {
   const [hasHydrated, setHasHydrated] = useState(false);
 
   useEffect(() => {
-    setHasHydrated(true);
+    const handle = requestAnimationFrame(() => {
+      setHasHydrated(true);
+    });
+    return () => cancelAnimationFrame(handle);
   }, []);
 
   const colorScheme = useRNColorScheme();
