84b31d1540db2f804be7cd8d97501da7d93ffc6c
Lukas Pikora <lpikora@gmail.com>
2026-08-23 20:18:07 +0200

    test: fix version numbers

--- src/stores/persist-version.ts
+++ src/stores/persist-version.ts
@@ -1,6 +1,10 @@
 import * as Application from "expo-application";
 
-export const APP_BUILD_NUMBER = Number(Application.nativeBuildVersion ?? 1);
+const parsedBuildNumber = Number(Application.nativeBuildVersion ?? 1);
+
+export const APP_BUILD_NUMBER = Number.isFinite(parsedBuildNumber)
+  ? parsedBuildNumber
+  : 1;
 
 export function discardPersistedOnVersionBump<T>(fresh: T) {
   return {
